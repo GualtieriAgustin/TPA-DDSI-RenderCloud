0317bb67bfed185c01ea7916944f3622b35f18ea
Gualtieri Agustin <agualtieri@frba.utn.edu.ar>
2025-11-25 21:14:07 -0300

    CloudFare

--- src/main/java/ar/edu/utn/frba/dds/controlador/CronController.java
+++ src/main/java/ar/edu/utn/frba/dds/controlador/CronController.java
@@ -5,13 +5,15 @@ import ar.edu.utn.frba.dds.persistencia.RepositorioDeColecciones;
 import ar.edu.utn.frba.dds.persistencia.RepositorioDeFuentes;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
-import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CronController {
 
     private final ServicioDeConsenso servicioDeConsenso;
     private final RepositorioDeFuentes repositorioDeFuentes;
     private final RepositorioDeColecciones repositorioDeColecciones;
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public CronController(ServicioDeConsenso servicioDeConsenso, RepositorioDeFuentes repositorioDeFuentes
             , RepositorioDeColecciones repositorioDeColecciones) {
@@ -27,11 +29,21 @@ public class CronController {
                 context.status(401).result("Unauthorized");
                 return;
             }
-            servicioDeConsenso.ponderarConsensoGlobal(repositorioDeFuentes.buscarTodas(), repositorioDeColecciones.consultarTodas());
-            context.status(200);
+            // Ejecutar en segundo plano
+            executor.submit(() -> {
+                try {
+                    ejecutarPonderacion();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
 
         } catch (Exception e) {
             context.status(400);
         }
+    }
+
+    public void ejecutarPonderacion(){
+        servicioDeConsenso.ponderarConsensoGlobal(repositorioDeFuentes.buscarTodas(), repositorioDeColecciones.consultarTodas());
     }
 }
